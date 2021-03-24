a6277ca3ad3c5735135f2323980daa8a77c28700
Crossy-swift <crossworm2011@gmail.com>
2021-03-24 19:27:17 +0300

    added logic

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    
+    @IBOutlet var yellowLightView: UIView!
+    
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var changeSignalButton: UIButton!
+    
+    private var buttonPressCount = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        greenLightView.layer.cornerRadius = 60
+        yellowLightView.layer.cornerRadius = 60
+        redLightView.layer.cornerRadius = 60
+        changeSignalButton.layer.cornerRadius = 15
+        
+        
     }
 
-
+    @IBAction func changeSignailButtonPressed() {
+        
+        switch buttonPressCount {
+        case 1:
+            greenLightView.alpha = 1
+        case 2:
+            greenLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+        case 3:
+            greenLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            redLightView.alpha = 1
+        default:
+            greenLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            redLightView.alpha = 0.3
+            buttonPressCount = 0
+        }
+        buttonPressCount += 1
+    }
+    
+    
 }
 
